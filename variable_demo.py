849f10d41c8130bf9ada6e880d76b8f01e634ea9
kfischer1 <kfischer2@babson.edu>
2016-09-06 14:04:25 -0400

    commit

--- variable_demo.py
+++ variable_demo.py
@@ -3,3 +3,10 @@ print('Hey Jude', "don't make it bad"")
 
 
 input()
+name = input()
+print('hello, ', name)
+
+messsage = 'Please enter your name: '
+print(message)
+name = input()
+print('Hello, ', name)
