06c3e4295d8be85b214200e5e63698d72b48b51e
kfischer1 <kfischer2@babson.edu>
2016-09-06 14:47:29 -0400

    commit

--- hello.py
+++ hello.py
@@ -3,13 +3,17 @@ print('Hello, world!')
 input()
 name = input()
 print('hello, ', name)
-
 messsage = 'Please enter your name: '
 print(message)
-name = input()
-print('Hello, ', name)
-
-
 
 input()
+
+a = 123 # a is an integer
+print(a)
+a = 'ABC' # a becomes a string
+print(a)
+
+x= 10
+x= x+2
+print(x)
 
