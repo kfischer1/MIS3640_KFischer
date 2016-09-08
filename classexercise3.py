e5ba2be6b0e0d1429effcad2ccedbeb75d83a003
kfischer1 <kfischer2@babson.edu>
2016-09-08 14:44:11 -0400

    commit

--- classexercise3.py
+++ classexercise3.py
@@ -1,5 +1,20 @@
 import time
-time.time()
+print(time.time())
+
+minutes = (time.time()/ 60)
+print(minutes)
+
+hours = (minutes / 60)
+print(hours)
+
+days = (hours / 24)
+print(days)
+
+years = (days / 365)
+print(years)
 
 
-input()
+
+
+
+
