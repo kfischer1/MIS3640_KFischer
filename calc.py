08acb5a025159f28d4f5bf91cbe5a90bf2135f7d
kfischer1 <kfischer2@babson.edu>
2016-09-08 14:11:46 -0400

    l

--- calc.py
+++ calc.py
@@ -7,8 +7,17 @@ print(60/5.633)
 
 print((4/3)*3.141592*5*3)
 
-print ((24.95 * .6) + (59*.75) + 3)
+#Question 2
+Price = 24.95 
+Discount = .6
+FirstShip = 3
+AdditionalShip = .75
+Amount = 60
 
+TotalCost = (Price * Discount * Amount) + FirstShip + (59 * AdditionalShip)
+print(TotalCost)
+
+#Question Three
 print((8.25*2) + (7.2*3))
 print(6 + (.52/60))
 print(6 * 60 + 52)
@@ -16,6 +25,7 @@ print ((6*60 + 52) + (38.1))
 print (450.1 / 60)
 print (7+(.50166*.60))
 
+#Question Four
 print('Increase: %d %%' %7)
 
 input()
