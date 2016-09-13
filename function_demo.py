ab8ecb83d9b8464f94468374224c5a9ce58e8985
kfischer1 <kfischer2@babson.edu>
2016-09-13 14:48:48 -0400

    commit

--- function_demo.py
+++ function_demo.py
@@ -10,8 +10,7 @@ def repeat_lyrics():
     print_lyrics()
 
 repeat_lyrics()
-    
-
+  #define function of print twice with 'a' of whatever is typed
 def print_twice(a):
     print(a)
     print(a)
@@ -20,6 +19,38 @@ print_twice('Babson')
 
 my_name = 'Kiley'
 print_twice(my_name)
+#prints name twice
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+
+line1= 'Bing tiddle'
+line2= 'tiddle bang.'
+#cat_twice(line1, line2)
+
+#print(cat)
+print(line1)
+
+def give_me_a_break():
+    str1 = 'break'
+    print('another break')
+    return str1
+
+new_str = give_me_a_break()
+print(new_str)
+    
+#give_me_a_break
+print(give_me_a_break())
+
+def a_new_function_demo():
+    s = 0
+    for x in 'Chris':
+        print(x)
+        print(ord(x))
+
+        
+
 
 
 input()
--- my_abs.py
+++ my_abs.py
@@ -1,1 +1,18 @@
-my_abs.py
+def my_abs(n):
+    if isinstance(n, int) or isinstance(n, float):
+        print('int')
+    if n > 0:
+        return n
+    else:
+        return -n
+    else:
+        print('invalid value')
+
+
+print (my_abs(100))
+
+print(my_abs(0))
+
+print(my_abs(-4))
+
+
