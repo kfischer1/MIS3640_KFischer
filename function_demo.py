2280b487fbb87ca08a4788908d64afea17931d44
kfischer1 <kfischer2@babson.edu>
2016-09-13 14:15:43 -0400

    commit functions

--- function_demo.py
+++ function_demo.py
@@ -1,10 +1,27 @@
 def print_lyrics():
-print("Hey Jude. Don't make it bad.")
+    print("Hey Jude. Don't make it bad.")
 print("Take a sad song and make it better.")
 
 print(type(print_lyrics))
+print_lyrics()
+def repeat_lyrics():
+    print_lyrics()
+    print('Na - na - na - na - na - na, na - na - na - na')
+    print_lyrics()
 
+repeat_lyrics()
+    
 
+def print_twice(a):
+    print(a)
+    print(a)
+
+print_twice('Babson')
+
+my_name = 'Kiley'
+print_twice(my_name)
 
 
 input()
+
+
