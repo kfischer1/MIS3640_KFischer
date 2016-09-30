de1c499445e710073b39b643b5db9190011a1625
kfischer1 <kfischer2@babson.edu>
2016-09-30 17:40:39 -0400

    class exercise
    upload s9

--- Session 9/Session9Exercise.py
+++ Session 9/Session9Exercise.py
@@ -1,2 +1,43 @@
-#Exercise 1
-#Write a program that reads words.txt and prints only the words with more than 20 characters (not counting whitespace).
+# #Exercise 2
+# #Using Recursion
+# def is_abecedarian(word):
+#     if len(word) <= 1:
+#         return True
+#     elif word[0] > word[1]:
+#         return False
+#     return(is_abecedarian(word[1:]))
+
+# print(is_abecedarian('abs'))
+# print(is_abecedarian('college'))
+
+#Using while loop
+# def is_abecedarian(word):
+#     n = 0
+#     previous = word[n]
+#     s = len(word)
+#     while n < s-1:
+#         if word[n + 1] < word[n]:
+#             return False
+#         n+=1
+#     return True
+
+# print(is_abecedarian('abs'))
+# print(is_abecedarian('college'))
+
+#Exercise 3
+# 3 pairs of letters consecutively
+def cattalk():
+    fin = open('words.txt')
+    for line in fin:
+        word = line.strip()
+        n = 0
+        while n <= (len(word)-6):
+            if word[n] == word[n+1]:
+                if word[n+2] == word[n+3]:
+                    if word [n+4] == word[n+5]:
+                        print(word)
+
+            n+=1
+
+cattalk()
+
